9ff259ebb7e50ba34da17a3b4298ab0242902675
ShubhamSaini142 <shubham.sms142@gmail.com>
2021-08-27 15:09:26 +0530

    .

---
+++ VideoPlayer/app/src/main/java/com/example/videoplayer/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.videoplayer
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.widget.MediaController
+import android.widget.VideoView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var videoView : VideoView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        videoView = findViewById(R.id.videoView)
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+        val uri:Uri = Uri.parse("https://assets.mixkit.co/videos/preview/mixkit-forest-stream-in-the-sunlight-529-large.mp4")
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+        videoView.requestFocus()
+        videoView.start()
+    }
+}
